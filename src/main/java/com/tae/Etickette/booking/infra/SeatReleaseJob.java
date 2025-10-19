b91bfe4dea11c28718bfff2dee820ad150c8baf1
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-19 19:54:36 +0900

    feat:예매 시 좌석 선점 시간 30분으로 제한하는 스케줄링

--- src/main/java/com/tae/Etickette/booking/infra/SeatReleaseJob.java
+++ src/main/java/com/tae/Etickette/booking/infra/SeatReleaseJob.java
@@ -1,7 +1,8 @@
-package com.tae.Etickette.bookseat.infra;
+package com.tae.Etickette.booking.infra;
 
 import com.tae.Etickette.bookseat.command.domain.BookSeatId;
 import com.tae.Etickette.bookseat.command.domain.SeatStatus;
+import com.tae.Etickette.bookseat.infra.BookSeatRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@DisallowConcurrentExecution
 public class SeatReleaseJob implements Job {
     private final BookSeatRepository bookSeatRepository;
     private final Scheduler scheduler;
@@ -23,14 +25,14 @@ public class SeatReleaseJob implements Job {
         bookSeatRepository.findById(new BookSeatId(seatId,sessionId)).ifPresent(seat->{
             if (seat.getStatus() == SeatStatus.LOCKED) {
                 seat.cancel();
-                log.info("delete success : status {},seatId {}",seat.getStatus(),seat.getSeatId());
             } else if (seat.getStatus() == SeatStatus.BOOKED) {
                 try {
                     scheduler.deleteJob(
                             JobKey.jobKey("seat-" + seatId + "-" + sessionId, "seat-release")
                     );
+                    log.info("스케줄 삭제 성공 job => {}", context.getJobDetail().getKey());
                 } catch (SchedulerException e) {
-                    throw new RuntimeException(e);
+                    log.info("스케줄 삭제 실패 job => {},{}", context.getJobDetail().getKey(),e.getMessage());
                 }
             }
         });
