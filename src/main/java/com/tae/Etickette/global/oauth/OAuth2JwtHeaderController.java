8ef0a4faff1531030ec7f35d83050ed03b91bd72
taeseonYoo <jkl0642@hansung.ac.kr>
2025-11-28 15:23:47 +0900

    feat: oauth2 인증 api swagger 설정

--- src/main/java/com/tae/Etickette/global/oauth/OAuth2JwtHeaderController.java
+++ src/main/java/com/tae/Etickette/global/oauth/OAuth2JwtHeaderController.java
@@ -1,16 +1,21 @@
 package com.tae.Etickette.global.oauth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "OAuth2 API", description = "OAuth2 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class OAuth2JwtHeaderController {
     private final OAuth2JwtHeaderService oauth2JwtHeaderService;
 
+    @Operation(summary = "OAuth2 AccessToken 헤더 전달 ",
+            description = "클라이언트의 access token 쿠키를 확인하고, Authorization 헤더에 토큰을 설정")
     @PostMapping("/oauth2-jwt-header")
     public void oauth2JwtHeader(HttpServletRequest request, HttpServletResponse response) {
         oauth2JwtHeaderService.oauth2JwtHeaderSet(request, response);
